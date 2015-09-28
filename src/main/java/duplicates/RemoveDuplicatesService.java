1a298cd04ce2c15e996bcd6211919cbc2b2af93d
Francisco H <Francisco.J.Hernandez@macys.com>
2015-09-28 18:45:29 -0300

    removing duplicates problem

---
+++ src/main/java/duplicates/RemoveDuplicatesService.java
@@ -0,0 +1,31 @@
+package com.awards.security;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RemoveDuplicatesService {
+
+
+    // write a method that returns a list of duplicates in a list of integers.
+    // input  -> list
+    // output -> list of duplicate number
+    public List<Integer> execute(List<Integer> numbers) {
+
+        Set<Integer> lookup = new HashSet<Integer>();
+        List<Integer> duplicates = new ArrayList<Integer>();
+
+        for(Integer number : numbers){  //O(n)
+            if(lookup.contains(number)) //O(1)
+            {
+                duplicates.add(number);
+            }else{
+                lookup.add(number);
+            }
+        }
+
+        return duplicates;
+    }
+}
